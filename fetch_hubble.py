feb94435ad2c689a15bbc655123ab402c81bcef9
Gleb Matrunich <gleb@matrunich.com>
2019-12-05 16:24:47 +0100

    Update fetch_hubble.py
    Уменьшил количество необходимых библиотек
    Избавился от закомментированного кода
    Использовал встроенную в Requests поддержку JSON
    Избававился от типов в названиях
    Использовал splitext из модуля os.path
    Сделал скрипт совместимым с *nix и Windows
    Упростил код в некоторых местах
    Подобрал более точные названия для переменных

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -1,6 +1,5 @@
 import requests
 import os
-import json
 import urllib3
 
 DIRECTORY_TO_SAVE_IMAGES = 'images'
@@ -10,43 +9,36 @@ HUBBLE_COLLECTION_NAME = 'wallpaper'
 
 def get_hubble_image_url(url):
     response = requests.request('GET', url)
-    #data = response.text
-    data = json.loads(response.text)
+    data = response.json()
     links_data = data['image_files']
-    links_list = []
+    links = []
     for link in links_data:
         link = "{}{}".format("https:", link['file_url'])
-        links_list.append(link)
-    return links_list[-1]
+        links.append(link)
+    return links[-1]
 
 
 def get_hubble_images_ids(collection_name):
     url = "{}{}{}".format(HUBBLE_URL, 'images/', collection_name)
     response = requests.request('GET', url)
-    #data = response.text
-    data = json.loads(response.text)
+    collection_info = response.json()
     ids_list = []
-    for item in data:
-        d = dict(item)
-        image_id = d['id']
+    for image in collection_info:
+        image_info = dict(image)
+        image_id = image_info['id']
         ids_list.append(image_id)
     return ids_list
-
-
-def get_image_type(url):
-    string = url.split('.')
-    return string[-1]
 
 
 def download_hubble_image(image_id):
     url = "{}{}{}".format(HUBBLE_URL, 'image/', image_id)
     response = requests.get(get_hubble_image_url(url), verify=False)
     image_url = response.url
+    image_type = list(os.path.splitext(image_url))
     response.raise_for_status()
-    filename = "{}/{}.{}".format(DIRECTORY_TO_SAVE_IMAGES, image_id, get_image_type(image_url))
-    if not os.path.exists(os.path.dirname(filename)):
-        dir_name = os.path.dirname(filename)
-        os.makedirs(dir_name)
+    filename = os.path.join(DIRECTORY_TO_SAVE_IMAGES, "{}{}".format(image_id, image_type[-1]))
+    if not os.path.exists(DIRECTORY_TO_SAVE_IMAGES):
+        os.makedirs(DIRECTORY_TO_SAVE_IMAGES)
     with open(filename, 'wb') as file:
         file.write(response.content)
 
