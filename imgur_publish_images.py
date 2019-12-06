6c182654d2e0b822a8cfcecd69546870f1ee792c
Gleb Matrunich <gleb@matrunich.com>
2019-12-06 10:00:49 +0100

    Update imgur_publish_images.py
    Убрал лишние пробелы.

--- imgur_publish_images.py
+++ imgur_publish_images.py
@@ -8,23 +8,22 @@ CROPPED_IMAGES_PREHEADER = 'cropped_'
 
 
 def authenticate():
-	client = ImgurClient(
-						os.environ['IMGUR_CLIENT_ID'], 
-						os.environ['IMGUR_CLIENT_SECRET'], 
-						os.environ['IMGUR_ACCESS_TOKEN'], 
-						os.environ['IMGUR_REFRESH_TOKEN']
-						)
-	return client
+    client = ImgurClient(os.environ['IMGUR_CLIENT_ID'], 
+                         os.environ['IMGUR_CLIENT_SECRET'], 
+                         os.environ['IMGUR_ACCESS_TOKEN'], 
+                         os.environ['IMGUR_REFRESH_TOKEN']
+			)
+    return client
 
 
 def send_images_to_imgur(client, directory):
     file_names = os.listdir(directory)
     not_cropped_images = filter(lambda x: not x.startswith(CROPPED_IMAGES_PREHEADER), file_names)
     for image in not_cropped_images:
-    	image = client.upload_from_path(os.path.join(directory, image), anon=False)
+        image = client.upload_from_path(os.path.join(directory, image), anon=False)
 
 
 if __name__ == "__main__":
-	load_dotenv()
-	client = authenticate()
-	send_images_to_imgur(client, DIRECTORY_WITH_IMAGES)
+    load_dotenv()
+    client = authenticate()
+    send_images_to_imgur(client, DIRECTORY_WITH_IMAGES)
